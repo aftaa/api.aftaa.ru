442d97ff662e11c66569ba67ad62d1bef050bfc5
Maxim Gabidullin <after@ya.ru>
2020-08-01 19:59:57 +0300

    commit

--- api/data/admin-data.php
+++ api/data/admin-data.php
@@ -1,15 +1,15 @@
 <?php
 
 $sql = 'SELECT l.id AS link_id, b.id AS block_id, l.name AS link_name, b.name AS block_name, '
-    . 'col_num, href, icon, b.private AS block_private, l.private AS link_private '
-    . 'FROM link l RIGHT JOIN link_block b ON l.block_id=b.id '
-    . 'WHERE b.deleted = FALSE AND l.deleted = FALSE '
-    . 'ORDER BY b.sort, l.name';
+     . 'col_num, href, icon, b.private AS block_private, l.private AS link_private '
+     . 'FROM link_block b LEFT JOIN link l ON l.block_id=b.id AND l.deleted = FALSE '
+     . 'WHERE b.deleted = FALSE ORDER BY b.sort, l.name';
 
 $rows = $app->pdo->query($sql);
 
 $data = [];
 while ($row = $rows->fetchObject()) {
+
     if (!array_key_exists($row->col_num, $data)) {
         $data[$row->col_num] = [];
     }
@@ -19,9 +19,7 @@ while ($row = $rows->fetchObject()) {
             'id'      => $row->block_id,
             'name'    => $row->block_name,
             'private' => (bool)$row->block_private,
-            'links'   => [
-
-            ],
+            'links'   => [],
         ];
     }
 
@@ -33,6 +31,7 @@ while ($row = $rows->fetchObject()) {
         'private' => (bool)$row->link_private,
     ];
 }
+
 
 return [
     'columns' => $data,
